3fb34dfa708d0472ce45f4f9af13e4b87f558f2e
juan pablo guzman moreno <juanpablo-gm@hotmail.com>
2023-12-13 14:29:34 -0500

    created list of products for frontend

--- scraping_shein.py
+++ scraping_shein.py
@@ -6,13 +6,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup
+import pprint
 import time
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 
 driver.get("https://us.shein.com/")
-driver.add_cookie({"name":"sessionID_shein","value":"s%3A9YbPM0_8AO5r-hGW8TW-5u5nu89OUQWk.hjEGt7sJYdeFMDOy%2F1ytFtt%2FCkPMkYIDZdQOc30qwdw"})
+driver.add_cookie({"name":"sessionID_shein","value":"s%3AAwee4OKP8XpUPE0qtiQf0JPVfzdnwjLk.Fa8nVe%2FFcZnY9HGXo0FFwaw0YYtXqB044keAR8pp99w"})
 
 try:
     wait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "icon-close"))).click()
@@ -28,22 +29,21 @@ products = wait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "j-
 products = products.get_attribute('innerHTML')
 soup = BeautifulSoup(products, 'html.parser')
     
-src_images = soup.find_all('div', class_="crop-image-container")
-    # print("esta es la caja de las imagenes:", src_images)
-    
-    # for element in src_images:
-    #      if element['src'].endswith(".png"):
-    #          print("estas son las imagenes:", element)
+cards = soup.find_all('section', class_='wish-list__item')
 
-for i, imagen in enumerate(src_images):    
-    if imagen['data-before-crop-src'].endswith('.jpg'):
-        print("la imagen", imagen['data-before-crop-src'])
-        r = requests.get(f"https:{imagen['data-before-crop-src']}")
-        
-        with open(f'imagen_1{i}.jpg', 'wb') as f:
-           f.write(r.content)
+list_products = []
 
+for element in cards:
+    name = element.find('a', class_='S-product-item__link').text
+    price = element.find('span', class_='normal-price-ctn__sale-price').text
+    image = element.find('div', class_='crop-image-container')['data-before-crop-src']
 
+    card_info = {'name': name, 'price': price, 'image': image}
+    list_products.append(card_info)
 
+    r = requests.get(f'https:{image}')
+    with open(f'{name}.jpg', 'wb') as f:
+        f.write(r.content)
 
-
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(list_products)    
